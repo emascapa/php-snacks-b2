9fe35873ff89643e638328a8caed58f2975a8567
emascapa <emascapa@gmail.com>
2022-05-23 12:59:25 +0200

    Add snack 1

---
+++ snack-1/index.php
@@ -0,0 +1,70 @@
+<?php 
+/* 
+Partendo da questo array: https://www.codepile.net/pile/Po60bjgQ
+
+Ad ogni refresh della pagina visualizzare una pubblicità a schermo, 
+tenendo conto che possono essere sorteggiate solo quelle is_active true.
+
+*/
+
+$ads = [
+    [
+        'image_path' => 'https://conversionsciences.com/wp-content/uploads/2019/04/example-of-emotional-logical-appeal-on-persuasive-copy.jpg',
+        'link' => 'https://www.google.com',
+        'is_active' => true,
+    ],
+    [
+        'image_path' => 'https://previews.123rf.com/images/hstrongart/hstrongart1612/hstrongart161200121/68060446-anuncios-de-cepillos-de-dientes-el%C3%A9ctricos-modo-diferente-de-este-producto-con-blanco-modelo-de-diente.jpg',
+        'link' => 'https://www.facebook.com',
+        'is_active' => true,
+    ],
+    [
+        'image_path' => 'https://thumbs.dreamstime.com/z/back-to-school-sale-background-chalkboard-sale-percentages-marketing-poster-color-pencils-96751666.jpg',
+        'link' => 'https://codepen.io',
+        'is_active' => false,
+    ],
+    [
+        'image_path' => 'https://foodsecurityfoodjustice.files.wordpress.com/2016/11/beyond-the-omlette-1.jpg',
+        'link' => 'https://laravel.com',
+        'is_active' => false,
+    ],
+    [
+        'image_path' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRniLfGdFLxVOUoEwYC9WeRIrfZInB74_80IV8yMnANx3HTZYoQ',
+        'link' => 'https://www.php.net',
+        'is_active' => true,
+    ],
+
+];
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+  </head>
+  <body class="bg-dark p-4">
+      <?php 
+      $ran_num = rand(0, count($ads)); 
+
+      while (!$ads[$ran_num]['is_active']) {
+        $ran_num = rand(0, count($ads));  
+      }
+      
+      ?>
+
+        <div class="bg-light p-3 rounded rounded-4 d-flex flex-column w-50 mx-auto text-center">
+            <img src="<?php echo $ads[$ran_num]['image_path']?>" alt="" class="img-fluid rounded rounded-4 mb-2">
+
+            <a href="<?php echo $ads[$ran_num]['link'] ?>"><?php echo $ads[$ran_num]['link'] ?></a>
+
+        </div>
+      
+  </body>
+</html>
